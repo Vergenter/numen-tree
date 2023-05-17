added example where code returns incorrect result

my_canonic.py
@@ -1,70 +1,6 @@
 import main_bfs
 import numpy as np
-from typing import List, Tuple
-
-
-def minimize_binary_value(arr: np.ndarray) -> Tuple[np.ndarray, List[int], List[int]]:
-    n, m = arr.shape
-    optimization_variants = [(list(range(n)), list(range(m)))]  # initial state
-    splits = [m]  # initial split at the end of the row
-
-    def count_zeros(row: np.ndarray, splits: List[int]) -> Tuple[int, ...]:
-        zeros = []
-        prev_split = 0
-        for split in splits:
-            zeros.append((row[prev_split:split] == 0).sum())
-            prev_split = split
-        return tuple(zeros)
-
-    def find_splits(row: np.ndarray, splits: List[int]) -> List[int]:
-        row_as_int = row.astype(int)
-        new_splits = list(np.where(np.diff(row_as_int) == 1)[0] + 1)
-        # merge and sort old and new splits
-        return sorted(list(set(splits + new_splits)))
-
-    for i in range(n):
-        new_variants = []
-        for rows_indices, columns_indices in optimization_variants:
-            # Rearrange the array according to the current indices
-            temp_arr = arr[np.ix_(rows_indices, columns_indices)]
-            zero_counts = [count_zeros(row, splits) for row in temp_arr[i:]]
-            max_zeros_rows = np.array([
-                idx + i for idx, zc in enumerate(zero_counts) if zc == max(zero_counts)])
-            # Get unique max_zeros_rows
-            _, idxs = np.unique(
-                temp_arr[max_zeros_rows], axis=0, return_index=True)
-            # Add all unique max_zeros_rows to new variants
-            for max_zeros_row in max_zeros_rows[idxs]:
-                new_rows_indices = rows_indices[:i] + [rows_indices[max_zeros_row]] + \
-                    rows_indices[i:max_zeros_row] + \
-                    rows_indices[max_zeros_row+1:]
-                new_variants.append((new_rows_indices, columns_indices))
-                if i > 0 and np.array_equal(temp_arr[new_rows_indices[i]], temp_arr[new_rows_indices[i-1]]):
-                    break
-        # Filter the new variants
-        max_zero_count = max_zero_count = max([count_zeros(arr[np.ix_(x[0], x[1])][i], splits)
-                                               for x in new_variants])
-        optimization_variants = [x for x in new_variants if count_zeros(
-            arr[np.ix_(x[0], x[1])][i], splits) == max_zero_count]
-        # print("variants after filter", len(optimization_variants))
-
-        # Update splits and sort columns
-        for j in range(len(optimization_variants)):
-            rows_indices, columns_indices = optimization_variants[j]
-            temp_arr = arr[np.ix_(rows_indices, columns_indices)]
-            for start, end in zip([0] + splits, splits + [m]):
-                # Sort columns in each part separately
-                part_indices = np.argsort(temp_arr[i, start:end]).tolist()
-                part_indices = [start + idx for idx in part_indices]
-                columns_indices[start:end] = [columns_indices[idx]
-                                              for idx in part_indices]
-            # call find_splits on sorted part of the array
-            splits = find_splits(temp_arr[i, columns_indices], splits)
-            optimization_variants[j] = (rows_indices, columns_indices)
-
-    # Return the best variant
-    best_rows_indices, best_columns_indices = optimization_variants[0]
-    return arr[np.ix_(best_rows_indices, best_columns_indices)], best_rows_indices, best_columns_indices
+import ordering.hungarianord as hunord
 
 
 def dag_to_biadjacency_matrix(graph: main_bfs.DAGSN) -> np.ndarray:
@@ -97,5 +33,5 @@ def dag_to_biadjacency_matrix(graph: main_bfs.DAGSN) -> np.ndarray:
 
 def get_canonical_form(graph: main_bfs.DAGSN) -> str:
     """Return the canonical form of the graph using pynauty."""
-    result = str(minimize_binary_value(dag_to_biadjacency_matrix(graph))[0])
+    result = str(hunord.get_minimized(dag_to_biadjacency_matrix(graph)))
     return result

ordering/hungarian.py
@@ -1,6 +1,5 @@
 from typing import Tuple
 import numpy as np
-import numpy as np
 
 
 def subtract_arrays(a: np.ndarray, b: np.ndarray, L: np.ndarray) -> np.ndarray:
@@ -26,16 +25,20 @@ def subtract_arrays(a: np.ndarray, b: np.ndarray, L: np.ndarray) -> np.ndarray:
 
 def add_arrays(a: np.ndarray, b: np.ndarray, L: np.ndarray) -> np.ndarray:
     result = np.zeros_like(a)
-    carry = np.zeros_like(a)
+    carry = 0
 
     for i in range(a.shape[0]-1, -1, -1):
-        summation = a[i] + b[i] + carry[i]
+        summation = a[i] + b[i] + carry
 
-        if summation > L[i]:
-            raise ValueError("Addition would result in value exceeding limit")
+        if summation >= L[i]:
+            carry = 1
+            result[i] = summation - L[i]
+        else:
+            carry = 0
+            result[i] = summation
 
-        carry[i-1] = summation // (L[i] + 1)
-        result[i] = summation % (L[i] + 1)
+    if carry > 0:
+        raise ValueError("Addition would result in value exceeding limit")
 
     return result
 
@@ -159,13 +162,13 @@ def solve_rectangular_linear_sum_assignment(cost_matrix: np.ndarray, maximize: b
         return np.arange(nr), col4row
 
 
-cost_matrix = np.array([
-    [[1, 2, 1], [2, 3, 1], [4, 1, 3]],
-    [[4, 5, 6], [11, 18, 3], [5, 4, 6]],
-    [[7, 5, 9], [8, 9, 7], [23, 7, 9]]
-])
+# cost_matrix = np.array([
+#     [[1, 2, 1], [2, 3, 1], [4, 1, 3]],
+#     [[4, 5, 6], [11, 18, 3], [5, 4, 6]],
+#     [[7, 5, 9], [8, 9, 7], [23, 7, 9]]
+# ])
 
-print(solve_rectangular_linear_sum_assignment(cost_matrix))
+# print(solve_rectangular_linear_sum_assignment(cost_matrix))
 # array([1, 2, 0]
 # [2, 3, 1]+[5, 4, 6]+[7, 5, 9]=14,12,16
 # x, 1, 2
@@ -173,3 +176,18 @@ print(solve_rectangular_linear_sum_assignment(cost_matrix))
 # 0, 2, 1 <=> [1, 2, 1]+[5, 4, 6]+[8, 9, 7]=14,15,14
 # 2, 0, 1 <=> [4, 1, 3]+[4, 5, 6]+[8, 9, 7]=16,15,16
 # 1, 2, 0 <=> [2, 3, 1]+[5, 4, 6]+[7, 5, 9]=14,12,16
+
+
+# cost_matrix = np.array([[[0, 0, 0, 2, 0, 0],
+#                          [0, 0, 0, 0, 2, 0],
+#                          [0, 0, 0, 0, 0, 2]],
+
+#                         [[1, 0, 0, 1, 0, 0],
+#                          [0, 1, 0, 0, 1, 0],
+#                          [0, 0, 1, 0, 0, 1]],
+
+#                         [[1, 0, 0, 1, 0, 0],
+#                          [0, 1, 0, 0, 1, 0],
+#                          [0, 0, 1, 0, 0, 1]]])
+
+# print(solve_rectangular_linear_sum_assignment(cost_matrix))